8666bd23d14411aaf255cda68bd9aede71763c59
Devanand Sharma <sharmadevanand99@gmail.com>
2020-06-14 16:40:03 +0530

    Added Login, Session functionality

--- bank.py
+++ bank.py
@@ -1,14 +1,73 @@
-from flask import Flask,render_template,url_for,request,redirect
+from flask import Flask,render_template,url_for,request,redirect, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 
 app=Flask(__name__)
 
-@app.route('/')
+# Secret key (can be anything, it's for extra protection)
+app.secret_key = 'thisismysecretkey123456789'
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'retailbank'
+
+# Intialize MySQL
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if password matches the criteria and then proceed
+        if re.match("(?=^.{10,}$)(?=.*\d)(?=.*[!@#$%^&*]+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$", password):
+            # Check if account exists using MySQL
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM userstore WHERE login = %s AND password = %s', (username, password,))
+            # Fetch one record and return result
+            account = cursor.fetchone()
+            # If account exists in accounts table in out database
+            if account:
+                # Create session data, we can access this data in other routes
+                session['loggedin'] = True
+                # session['id'] = account['id']
+                session['login'] = account['login']
+                # Redirect to home page
+                # msg = 'Logged in successfully!'
+                return redirect(url_for('home'))
+            else:
+                # Account doesnt exist or username/password incorrect
+                msg = 'Incorrect username/password !!'
+        else:
+            msg = 'Password should contain one special character, one upper case, one numeric.'
+
+    return render_template("login.html", msg=msg)
+
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   # Redirect to login page
+   return redirect(url_for('login'))
 
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['login'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 
 @app.route('/create_customer_screen')
 def create_customer_screen():
