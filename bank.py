ac8bee13ff976a3d42463e4d0decec576b0cd951
Devanand Sharma <sharmadevanand99@gmail.com>
2020-06-14 17:11:14 +0530

    Added Login, Session functionality

--- bank.py
+++ bank.py
@@ -21,6 +21,11 @@ mysql = MySQL(app)
 def login():
     # Output message if something goes wrong...
     msg = ''
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['login'])
+        
     # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         # Create variables for easy access
